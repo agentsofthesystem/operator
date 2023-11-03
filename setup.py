0234afbe870745461b4d99b0a7171dcc334e1f67
Joshua Reed <11220408+jreed1701@users.noreply.github.com>
2023-11-03 15:43:00 -0600

    Apply black formatting.

--- setup.py
+++ setup.py
@@ -4,22 +4,21 @@ import sysconfig
 
 from setuptools import setup, find_packages
 
-PACKAGE_NAME = 'operator'
-PACKAGE_DIRECTORY = 'operator_client'
+PACKAGE_NAME = "operator"
+PACKAGE_DIRECTORY = "operator_client"
 SITE_PACKAGES_DIR = sysconfig.get_paths()["purelib"]
 
-with pathlib.Path('requirements.txt').open() as requirements_txt:
+with pathlib.Path("requirements.txt").open() as requirements_txt:
     install_requires = [
         str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
     ]
 
 setup(
     name=PACKAGE_NAME,
-    version='0.1.0',
-    description='Python Client to AgentSmith',
+    version="0.1.0",
+    description="Python Client to AgentSmith",
     install_requires=install_requires,
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
 )
